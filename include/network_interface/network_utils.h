5af119c0f0a7d6706be9fad37232cbe965e07078
Joshua Whitley <jwhitley@autonomoustuff.com>
2019-06-03 11:25:13 -0500

    Moving definition of return_status_desc to fix linking bug.

--- include/network_interface/network_utils.h
+++ include/network_interface/network_utils.h
@@ -246,28 +246,6 @@ void parse_tuple(
   }
 };
 
-std::string return_status_desc(const ReturnStatuses &ret)
-{
-  if (ret == ReturnStatuses::INIT_FAILED)
-    return "Initialization of the network interface failed.";
-  else if (ret == ReturnStatuses::BAD_PARAM)
-    return "A bad parameter was provided to the network interface during initalization.";
-  else if (ret == ReturnStatuses::SOCKET_ERROR)
-    return "A socket error was encountered.";
-  else if (ret == ReturnStatuses::SOCKET_CLOSED)
-    return "Socket is not currently open.";
-  else if (ret == ReturnStatuses::NO_MESSAGES_RECEIVED)
-    return "No messages were received on the interface.";
-  else if (ret == ReturnStatuses::READ_FAILED)
-    return "A read operation failed on the network interface.";
-  else if (ret == ReturnStatuses::WRITE_FAILED)
-    return "A write operation failed on the network interface.";
-  else if (ret == ReturnStatuses::CLOSE_FAILED)
-    return "Closing the network failed.";
-  else
-    return "Undefined error.";
-}
-
 }  // namespace Network
 }  // namespace AS
 
